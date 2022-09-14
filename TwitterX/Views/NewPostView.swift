8ea07a7c4233c208994332278ded5060d128a9a0
Xavier <xt.icloud@icloud.com>
2022-09-14 01:50:14 -0400

    created new post view

--- TwitterX/Views/NewPostView.swift
+++ TwitterX/Views/NewPostView.swift
@@ -6,10 +6,94 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct NewPostView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.colorScheme) var colorScheme
+    @State private var selectedImages: [PhotosPickerItem] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                    print("Dismissed.")
+                }, label: {
+                    Text("Cancel")
+                        .font(.subheadline)
+                        .foregroundColor(.accentColor)
+                        .padding(.leading, 30)
+                })
+                Spacer()
+                Button(action: {
+                    print("tweeted.")
+                    // create post
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    WideButtonLabel(
+                        text: "Tweet",
+                        fgColor: (colorScheme == .dark) ? .accentColor : .white,
+                        bgColor: (colorScheme == .dark) ? .white : .accentColor,
+                        width: 100
+                    )
+                        .padding(.trailing, 30)
+                })
+            }
+            .padding(.top, 50)
+            
+            HStack {
+                VStack {
+                    //ProfileImageView(name: currentUser)
+                    //.scaleEffect(0.8)
+                    Spacer()
+                }
+                VStack {
+                    TextField("What’s happending?", text: .constant(""),  axis: .vertical)
+                        .lineLimit(5...10)
+                        .frame(width: 210)
+
+                    Spacer()
+                }
+            }
+            .padding(.trailing, 30)
+            .padding(.top, 20)
+            Spacer()
+            Text("Error message placeholder")
+                .foregroundColor(.errorRed)
+            Divider()
+            HStack {
+                PhotosPicker(
+                    selection: $selectedImages,
+                    maxSelectionCount: 1,
+                    matching: .images,
+                    label: {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .bold()
+                            .foregroundColor(.accentColor)
+                            .scaledToFit()
+                            .frame(width: 27, height: 16)
+                    })
+                Image(systemName: "video")
+                    .resizable()
+                    .bold()
+                    .foregroundColor(.accentColor)
+                    .scaledToFit()
+                    .frame(width: 27, height: 16)
+                
+                Image(systemName: "location")
+                    .resizable()
+                    .bold()
+                    .foregroundColor(.accentColor)
+                    .scaledToFit()
+                    .frame(width: 27, height: 16)
+                Spacer()
+            }
+            .padding(.leading, 49)
+            .padding(.top, 5)
+            .padding(.bottom, 30)
+        }
     }
 }
 
